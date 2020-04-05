cfd854fb497745340f89376cdfcb4c1ffcd6796c
Todd Hastings <jeremy.t.hastings@gmail.com>
2020-04-05 21:46:00 +0000

    Updated dns_server playbook to provide DNS packet capture service

--- packetcap.sh
+++ packetcap.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
 
-tshark -i eth0 -F pcap -w "/home/ec2-user/${DATE}.pcap" -f "src port 53"
+tshark -i eth0 -F pcap -w "/tmp/${DATE}.pcap" -f "src port 53"
